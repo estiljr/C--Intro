d61b59f67aa1ccea3a8f0a4ce5152db02de9733f
Jennyl <estil.jennyl@yahoo.com>
2013-06-27 14:29:53 +0100

    working fine.. compilation to 46bit needs to be done.

--- GridToXYZ/GridToXYZ.cpp
+++ GridToXYZ/GridToXYZ.cpp
@@ -26,20 +26,22 @@ int main(int argc, const char* argv[])
 		double yllcorner = atof(LineQuery[3].c_str());
 		double dx = atof(LineQuery[4].c_str());
 		int nodata = atoi(LineQuery[5].c_str());
-		outputFile << "ncols ,"<< ncols<< endl;					cout << "ncols: "<< ncols<< endl; 
-		outputFile << "nrows ," << nrows << endl;				cout << "nrows: " << nrows << endl; 
-		outputFile << "xllcorner ," << xllcorner << endl;		cout << "xllcorner: " << xllcorner << endl;
-		outputFile << "yllcorner ," << yllcorner << endl;		cout << "yllcorner: " << yllcorner << endl;
-		outputFile << "cellsize ," << dx << endl;				cout << "cellsize: " << dx << endl;
-		outputFile << "nodata ," << nodata << endl;				cout << "nodata: " << nodata << endl;
-		outputFile << "x"<<","<<"y"<<","<<"z"<<endl;
-		cout<<"Reading input Grid file..."<<endl;
+		outputFile << "ncols ,"<< ncols<< "\n";					cout << "ncols: "<< ncols<< "\n"; 
+		outputFile << "nrows ," << nrows << "\n";				cout << "nrows: " << nrows << "\n"; 
+		outputFile << "xllcorner ," << xllcorner << "\n";		cout << "xllcorner: " << xllcorner << "\n";
+		outputFile << "yllcorner ," << yllcorner << "\n";		cout << "yllcorner: " << yllcorner << "\n";
+		outputFile << "cellsize ," << dx << "\n";				cout << "cellsize: " << dx << "\n";
+		outputFile << "nodata ," << nodata << "\n";				cout << "nodata: " << nodata << "\n";
+		outputFile << "i-index ,"<<"j-index ,"<<"x ,"<<"y ,"<<"z"<<"\n";
+		cout<<"Reading input Grid file..."<<"\n";
 
 		string** arr = new string*[nrows];
 		for(int a = 0; a < nrows; a++)
 			arr[a] = new string[ncols];
 		double* x = new double[nrows];
 		double* y = new double[ncols];
+		int* i_index = new int[nrows];
+		int* j_index = new int[ncols];
 
 		for (int i = nrows-1; i >=0; i--){
 			for (int j = 0; j < ncols; j++){
@@ -49,23 +51,28 @@ int main(int argc, const char* argv[])
 					getline(inputFile,arr[i][j],' ');
 			}
 		} 
-		cout << "Converting Grid to XYZ..." << endl;
+
+		cout << "Converting Grid to XYZ..." << "\n";
 		for (int m = 0; m < nrows; m++){
 			for(int n = 0; n < ncols; n++){
-				x[n] = double (xllcorner + dx*n + 0.5*dx);
-				y[m] = double (yllcorner + dx*m + 0.5*dx);
-				x[0] = 0.5*dx + dx*m;
-				cout<<x[n]<<','<<y[m]<<','<<arr[m][n]<<'\n';
+				x[n] = xllcorner + dx*n + 0.5*dx;
+				y[m] = yllcorner + dx*m + 0.5*dx;
+				j_index[n] = 1+n;
+				i_index[m] = 1+m;
+				outputFile.precision(10);
+				outputFile<<i_index[m]<<','<<j_index[n]<<','<<x[n]<<','<<y[m]<<','<<arr[m][n]<<'\n';
 			}
 		}
+
+		cout<<"Deallocating memory..."<< "\n";
 		for (int i = 0; i < nrows; i++)
 			delete [] arr[i];
-		delete [] arr; delete [] x; delete [] y;
+		delete [] arr; //delete [] x; delete [] y;
 		inputFile.close();
 		outputFile.close();
 	}
 	else cout << "The input file is not open!";
-	cout << "Process complete! Press any key to continue..." << endl;
+	cout << "Process complete! Press any key to continue..." << "\n";
 	_getch();
 	return 0;
 }
