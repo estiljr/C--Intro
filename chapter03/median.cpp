18720870bc77ae46e109261dad99761b8eec65c6
Jennyl <estil.jennyl@yahoo.com>
2013-07-05 00:02:01 +0100

    final chapter 03

--- chapter03/median.cpp
+++ chapter03/median.cpp
@@ -2,10 +2,12 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
+#include <iomanip>
 using namespace std;
 
 int main()
-{dz
+{
 	// ask for and read the student's name
 	cout << "Please enter your first name: ";
 	string name;
@@ -14,22 +16,41 @@ int main()
 
 	// ask for and read the midterm and final grades
 	cout << "Please enter your midterm and final exam grades: ";
-	double median, final;
-	cin >> median >> final;
+	double medterm, final;
+	cin >> medterm >> final;
 
 	// ask for and read the homework grades
 	cout << "Enter all your homework grades, "
-		"and write end at the end: ";
+		"and write 'end' at the end: ";
 	
 	double x;
 	vector <double> homework;
-
-	//invariant: homework contains all the homework grades read so far
+	// invariant: homework contains all the homework grades read so far
 	while (cin >> x)
 		homework.push_back(x);
 
-	typedef vector<double>::size_type vec_sz;
-	vec_sz size = homework.size();
+	// check that the student entered some homework grades
+	typedef vector<double>::size_type vector_size;
+	vector_size size = homework.size();
+	if (size == 0){
+		cout << endl << "You must enter your grades. " 
+			"Please try again." << endl;
+		return 1;
+	}
+
+	// sort the grades
+	sort(homework.begin(), homework.end());
+
+	// compute the median homework grade
+	vector_size mid = size/2;
+	double median = size % 2 == 0 ? (homework[mid]+homework[mid-1])/2 : homework[mid];
+	cout << "Your median score is " << median << endl;
+
+	// compute and write the final grade
+	streamsize prec = cout.precision();
+	cout << "Your final grade is " << setprecision(3)
+		<< 0.2*medterm + 0.4*final + 0.4*median 
+		<< setprecision(prec) << endl << endl;;
 
 	return 0;
 }
